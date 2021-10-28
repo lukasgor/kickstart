6c4795de8f84a2d02db0fb1477cbffca0b5b68bb
lukasgor <lukasz@acast.com>
2021-10-28 17:07:10 +0200

    fix: add window declaration

--- ethereum/web3.ts
+++ ethereum/web3.ts
@@ -4,6 +4,9 @@ let web3;
 const ROPSTEN_RPC_URL =
   'https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare let window: any;
+
 if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
   // We are in the browser and metamask is running.
   window.ethereum.request({ method: 'eth_requestAccounts' });
